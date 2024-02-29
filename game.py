47bee2145f2aae02047fbb8d26a5512c231e9301
H1JIK <konorfox@gmail.com>
2024-03-01 01:17:21 +0500

    version 1, beta, add game

---
+++ game.py
@@ -0,0 +1,112 @@
+import pygame
+from random import randint
+
+pygame.mixer.pre_init(44100, -16, 1, 512)
+pygame.init()
+pygame.time.set_timer(pygame.USEREVENT, 2000)
+pygame.mixer.music.load('sounds/music_fon.mp3')
+pygame.mixer.music.play(-1)
+
+s_catch = pygame.mixer.Sound('sounds/catch.mp3')
+
+BLACK = (0, 0, 0)
+W, H = 1920, 980
+
+sc = pygame.display.set_mode((W, H))
+
+clock = pygame.time.Clock()
+FPS = 60
+
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, x, speed, surf, score, group):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = surf
+        self.rect = self.image.get_rect(center=(x, 0))
+        self.speed = speed
+        self.score = score
+        self.add(group)
+
+    def update(self, *args):
+        if self.rect.y < args[0] - 20:
+            self.rect.y += self.speed
+        else:
+            self.kill()
+
+
+def collideBalls():
+    global game_score
+    for ball in balls:
+        if t_rect.collidepoint(ball.rect.center):
+            s_catch.play()
+            game_score += ball.score
+            ball.kill()
+
+
+def createBall(group):
+    indx = randint(0, len(balls_surf) - 1)
+    x = randint(20, W - 20)
+    speed = randint(1, 4)
+
+    return Ball(x, speed, balls_surf[indx], balls_data[indx]['score'], group)
+
+
+score = pygame.image.load('images/score_fon.png').convert_alpha()
+f = pygame.font.SysFont('arial black', 20)
+
+telega = pygame.image.load('images/telega.png').convert_alpha()
+t_rect = telega.get_rect(centerx=W // 2, bottom=H - 5)
+
+balls_data = ({'path': 'bronze_coin.png', 'score': 100},
+              {'path': 'silver_coin.png', 'score': 150},
+              {'path': 'gold_coin.png', 'score': 200})
+
+balls_surf = [pygame.image.load('images/' + data['path']).convert_alpha() for data in balls_data]
+
+game_score = 0
+
+balls = pygame.sprite.Group()
+
+bg = pygame.image.load('images/background.png').convert()
+
+speed = 10
+createBall(balls)
+
+def start():
+    pygame.mixer.pre_init(44100, -16, 1, 512)
+    pygame.init()
+    pygame.time.set_timer(pygame.USEREVENT, 2000)
+
+    pygame.mixer.music.load('sounds/music_fon.mp3')
+    pygame.mixer.music.play(-1)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+            elif event.type == pygame.USEREVENT:
+                createBall(balls)
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            t_rect.x -= speed
+            if t_rect.x < 0:
+                t_rect.x = 0
+        elif keys[pygame.K_RIGHT]:
+            t_rect.x += speed
+            if t_rect.x > W - t_rect.width:
+                t_rect.x = W - t_rect.width
+
+        collideBalls()
+
+        sc.blit(bg, (0, 0))
+        sc.blit(score, (0, -65))
+        sc_text = f.render(f'Счет: {str(game_score)}', 1, (94, 138, 14))
+        sc.blit(sc_text, (35, 20))
+
+        balls.draw(sc)
+        sc.blit(telega, t_rect)
+        pygame.display.update()
+        pygame.display.set_caption(f'Catch a coin|Score: {game_score}')
+        clock.tick(FPS)
+
+        balls.update(H)
---
+++ main.py
@@ -0,0 +1,101 @@
+import pygame
+import sys
+from button import ImageButton
+from game import start
+
+pygame.init()
+pygame.mixer.music.load('sounds/menu_music.mp3')
+pygame.mixer.music.play(-1)
+W, H = 1920, 980
+menu_background = pygame.image.load('images/background_menu.jpg')
+
+sc = pygame.display.set_mode((W, H))
+pygame.display.set_caption('Catch a coin|Menu')
+clock = pygame.time.Clock()
+FPS = 60
+
+def main_menu():
+    start_but = ImageButton(W/2-(300/2), 350, 300, 150, '', 'images/start1.png', 'images/start2.png', 'sounds/menu_click.mp3')
+    settings_but = ImageButton(W / 2 - (300 / 2), 500, 300, 150, 'Настройки', 'images/title1.png', 'images/title2.png', 'sounds/menu_click.mp3')
+    exit_but = ImageButton(W / 2 - (300 / 2), 650, 300, 150, 'Выход', 'images/exit1.png', 'images/exit2.png', 'sounds/menu_click.mp3')
+
+    running = True
+    while running:
+        sc.fill((0, 0, 0))
+        sc.blit(menu_background, (0, 0))
+
+        font = pygame.font.SysFont('Arial Black', 72)
+        text_surface = font.render('CATCH A COIN', True, (255, 255, 255))
+        text_rect = text_surface.get_rect(center=(W/2, 100))
+        sc.blit(text_surface, text_rect)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+                sys.exit()
+            try:
+                if event.type == pygame.USEREVENT:
+                    if event.button == start_but:
+                        start()
+
+                if event.type == pygame.USEREVENT and event.button == settings_but:
+                    settings_menu()
+
+                if event.type == pygame.USEREVENT and event.button == exit_but:
+                    sys.exit(0)
+
+            except AttributeError:
+                continue
+
+            for btn in [start_but, settings_but, exit_but]:
+                btn.handle_event(event)
+
+        for btn in [start_but, settings_but, exit_but]:
+            btn.check_hover(pygame.mouse.get_pos())
+            btn.draw(sc)
+
+        pygame.display.flip()
+
+def settings_menu():
+    audio_button = ImageButton(W / 2 - (300 / 2), (H/2), 300, 150, '', 'images/sound_but_on.png', 'images/sound_but_off.png', 'sounds/menu_click.mp3')
+    back_button = ImageButton(W / 2 - (100 / 2), 800, 100, 100, '<-', 'images/back_btn1.png', 'images/back_btn2.png', 'sounds/menu_click.mp3')
+
+
+    running = True
+    while running:
+        sc.fill((0, 0, 0))
+        sc.blit(menu_background, (0, 0))
+
+        font = pygame.font.SysFont('Arial Black', 72)
+        text_surface = font.render('Settings (в разработке)', True, (255, 255, 255))
+        text_rect = text_surface.get_rect(center=(W / 2, 100))
+        sc.blit(text_surface, text_rect)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+                sys.exit()
+            try:
+                if event.type == pygame.USEREVENT:
+                    if event.button == audio_button:
+                        pass
+
+                    if event.button == back_button:
+                        main_menu()
+
+            except AttributeError:
+                continue
+
+            for btn in [audio_button, back_button]:
+                btn.handle_event(event)
+
+        for btn in [audio_button, back_button]:
+            btn.check_hover(pygame.mouse.get_pos())
+            btn.draw(sc)
+
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    main_menu()
